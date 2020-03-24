bb40c78982b860d914abdbbbf1445ec45756d96b
Catherine Jimenez <xcatherine.jimenez@gmail.com>
2020-03-24 17:40:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
-var kittens = ["Milo", "Otis", "Garfield"];
